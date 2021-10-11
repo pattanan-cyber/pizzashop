f449ee935c9ca413db9b15df3179e338a65283fb
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2021-10-11 11:47:48 +0700

    refactoring code

--- pizza.py
+++ pizza.py
@@ -1,24 +1,46 @@
+from enum import Enum
+
+class PizzaSize(Enum):
+    # Enum members written as: name = value
+    small = 120
+    medium = 200
+    large = 320
+    jumbo = 450
+
+    def __str__(self):
+        return self.name
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
-    def __init__(self, size):
+    def __init__(self, size: PizzaSize):
+        if not isinstance(size, PizzaSize):
+            raise TypeError("Size mist be a Pizzasize")
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
-        return price
+        return self.size.value + 20*len(self.toppings)
     
-    def addTopping(self, topping):
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
+
+    def __str__(self):
+        description = self.size.name
+        if self.toppings:
+            description += " pizza with " + ", ".join(self.toppings)
+        else:
+            description += " plain cheeze pizza"
+        return description
+
+
+
+
+
+
+
+
 
